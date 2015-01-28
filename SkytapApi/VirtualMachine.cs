973632a59e6a6abf7ae47f8f997a13a36cba98bc
gjanco <gjanco@gmail.com>
2015-01-28 09:14:06 -0500

    missing a file

---
+++ SkytapApi/VirtualMachine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkytapApi
+{
+    public class VirtualMachine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NatIP { get; set; }
+    }
+}
